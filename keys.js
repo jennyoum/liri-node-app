2b8bc382c02a9d64f02ed595a069fbdae2e0bb27
Jenny Oum <jennyoum@hotmail.com>
2019-07-31 13:55:44 -0400

    completed movie search and concert search

--- keys.js
+++ keys.js
@@ -1,9 +1,10 @@
 console.log('this is loaded');
-
+require("dotenv").config();
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
 var keys = require("./keys.js");
+
 var spotify = new Spotify(keys.spotify);
-concert-this
+// Spotify Client ID b6dd004bef0d47ee9624512c9bdc7f4c
--- liri.js
+++ liri.js
@@ -1,1 +1,87 @@
 require("dotenv").config();
+var axios = require("axios");
+var spotify = require('node-spotify-api');
+
+switch(process.argv[2]){
+    case 'concert-this':
+        concertSearch();
+        break;
+
+    case 'spotify-this-song':
+        songSearch();
+        break;
+
+    case 'movie-this':
+        movieSearch();
+        break;
+
+    case 'do-what-it-says':
+        whatever();
+        break;
+};
+
+// Concert section
+function concertSearch(){
+var band = process.argv[3];
+
+    axios.get("https://rest.bandsintown.com/artists/"+band+"/events?app_id=codingbootcamp").then(
+        function(response){
+            console.log(`Your band results:
+            Venue Name: ${response.data[0].venue['name']}
+            Venue Location: ${response.data[0].venue['city']}
+            Date: ${response.data[0].datetime}
+            `);
+        }
+    );
+};
+
+// Spotify section
+function songSearch(){
+    var song = process.argv[3];
+  
+  spotify.search({ type: 'track', query: 'wannabe' }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+  
+  console.log(data); 
+  });
+}
+
+// Movie section
+function movieSearch(){
+    var movie = process.argv[3];
+
+    axios.get("http://www.omdbapi.com/?t="+movie+"&y=&plot=short&apikey=trilogy").then(
+    function(response) {
+        console.log (`The movie's search results: 
+        
+        Title: ${response.data.Title},
+        Release date: ${response.data.Released},
+        Rating: ${response.data.imdbRating},
+        Rotten Tomatoes Rating: ${response.data.imdbRating[1]},
+        Country Produced: ${response.data.Country},
+        Language: ${response.data.Language},
+        Plot: ${response.data.Plot},
+        Actors: ${response.data.Actors}
+        ` );
+    })
+    .catch(function(error) {
+        if (error.response) {
+        
+        console.log("---------------Data---------------");
+        console.log(error.response.data);
+        console.log("---------------Status---------------");
+        console.log(error.response.status);
+        console.log("---------------Status---------------");
+        console.log(error.response.headers);
+        } else if (error.request) {
+
+        console.log(error.request);
+        } else {
+
+        console.log("Error", error.message);
+        }
+        console.log(error.config);
+    });
+}
